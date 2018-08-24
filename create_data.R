99d5c3d8e8ffb5616f835ccb3980d802e11f1ded
Alex Miller <alex.k.miller@gmail.com>
2018-08-24 09:51:58 -0400

    Refactoring code

--- create_data.R
+++ create_data.R
@@ -61,87 +61,16 @@ p4a$sector_percent_of_bundle = 100*(p4a$usd_disb/p4a$usd_disb_bundle_total)
 p4a = p4a[order(p4a$recipient_name,p4a$itep_sector,-p4a$sector_percent)]
 
 #donor p4 starts here####
-
-p4b_donor = data.table(crs)[
-  ,.(usd_disb=sum(usd_disbursement,na.rm=T))
-  ,by=.(donor_name,itep_sector,oda_bundle_name)
-]
-p4b_donor = p4b_donor[order(p4b_donor$donor_name,p4b_donor$itep_sector,-p4b_donor$usd_disb)]
-
-# counting top sectors by bundle ####
-p4b_donor$donor_sector = paste(p4b_donor$donor_name,p4b_donor$itep_sector)
-top10_sector_donor = data.table(crs)[
-  ,.(usd_disb=sum(usd_disbursement,na.rm=T))
-  ,by=.(donor_name,itep_sector)
-  ]
-top10_sector_donor = top10_sector_donor[order(top10_sector_donor$donor_name,-top10_sector_donor$usd_disb)]
-
-top10_sector_donor = top10_sector_donor[,head(.SD,10),by=.(donor_name)]
-valid_combinations = paste(top10_sector_donor$donor_name,top10_sector_donor$itep_sector)
-
-
-p4b_donor_top1_sector = subset(p4b_donor,donor_sector %in% valid_combinations)
-p4b_donor_top1_sector$dup = duplicated(data.frame(p4b_donor_top1_sector)[,c("donor_name","itep_sector")])
-p4b_donor_top1_sector = subset(p4b_donor_top1_sector,!dup)
-p4b_donor_top1_sector = merge(p4b_donor_top1_sector,top10_sector_donor,all=FALSE)
-p4b_donor_top1_sector$count = 1
-p4b_donor_top1_sector_count = p4b_donor_top1_sector[,.(top1count=sum(count)),by=.(donor_name,oda_bundle_name)]
-p4b_donor_top3_sector = subset(p4b_donor,donor_sector %in% valid_combinations)
-p4b_donor_top3_sector = p4b_donor_top3_sector[,head(.SD,3),by=.(donor_name,itep_sector)]
-p4b_donor_top3_sector$count = 1
-p4b_donor_top3_sector_count = p4b_donor_top3_sector[,.(top3count=sum(count)),by=.(donor_name,oda_bundle_name)]
-p4b_donor_top_sector = merge(p4b_donor_top1_sector_count,p4b_donor_top3_sector_count,all=TRUE)
-
-# counting top bundles by sector ####
-p4b_donor$donor_bundle = paste(p4b_donor$donor_name,p4b_donor$oda_bundle_name)
-top10_bundle_donor = data.table(crs)[
-  ,.(usd_disb=sum(usd_disbursement,na.rm=T))
-  ,by=.(donor_name,oda_bundle_name)
-  ]
-top10_bundle_donor = top10_bundle_donor[order(top10_bundle_donor$donor_name,-top10_bundle_donor$usd_disb)]
-
-top10_bundle_donor = top10_bundle_donor[,head(.SD,10),by=.(donor_name)]
-valid_combinations = paste(top10_bundle_donor$donor_name,top10_bundle_donor$oda_bundle_name)
-
-
-p4b_donor_top1_bundle = subset(p4b_donor,donor_bundle %in% valid_combinations)
-p4b_donor_top1_bundle$dup = duplicated(data.frame(p4b_donor_top1_bundle)[,c("donor_name","oda_bundle_name")])
-p4b_donor_top1_bundle = subset(p4b_donor_top1_bundle,!dup)
-p4b_donor_top1_bundle = merge(p4b_donor_top1_bundle,top10_bundle_donor,all=FALSE)
-p4b_donor_top1_bundle$count = 1
-p4b_donor_top1_bundle_count = p4b_donor_top1_bundle[,.(top1count=sum(count)),by=.(donor_name,itep_sector)]
-p4b_donor_top3_bundle = subset(p4b_donor,donor_bundle %in% valid_combinations)
-p4b_donor_top3_bundle = p4b_donor_top3_bundle[,head(.SD,3),by=.(donor_name,oda_bundle_name)]
-p4b_donor_top3_bundle$count = 1
-p4b_donor_top3_bundle_count = p4b_donor_top3_bundle[,.(top3count=sum(count)),by=.(donor_name,itep_sector)]
-p4b_donor_top_bundle = merge(p4b_donor_top1_bundle_count,p4b_donor_top3_bundle_count,all=TRUE)
-
-#recipient p4 starts here ####
-p4b_recipient = data.table(crs)[
-  ,.(usd_disb=sum(usd_disbursement,na.rm=T))
-  ,by=.(recipient_name,donor_name,oda_bundle_name)
-  ]
-p4b_recipient = p4b_recipient[order(p4b_recipient$recipient_name,p4b_recipient$donor_name,-p4b_recipient$usd_disb)]
-p4b_recipient$recipient_sector = paste(p4b_recipient$recipient_name,p4b_recipient$donor_name)
-top10_donor_sector = data.table(crs)[
-  ,.(usd_disb=sum(usd_disbursement,na.rm=T))
-  ,by=.(recipient_name,donor_name)
-  ]
-top10_donor_sector = top10_donor_sector[order(top10_donor_sector$recipient_name,-top10_donor_sector$usd_disb)]
-
-top10_donor_sector = top10_donor_sector[,head(.SD,10),by=.(recipient_name)]
-valid_combinations = paste(top10_donor_sector$recipient_name,top10_donor_sector$donor_name)
-
-p4b_recipient_top1_bundle= subset(p4b_recipient,recipient_sector %in% valid_combinations)
-p4b_recipient_top1_bundle = p4b_recipient_top1_bundle[,head(.SD,1),by=.(recipient_name,donor_name)]
-p4b_recipient_top1_bundle$count = 1
-p4b_recipient_top1_bundle_count = p4b_recipient_top1_bundle[,.(top1count=sum(count)),by=.(recipient_name,oda_bundle_name)]
-
-p4b_recipient_top3_bundle = subset(p4b_recipient,recipient_sector %in% valid_combinations)
-p4b_recipient_top3_bundle = p4b_recipient_top3_bundle[,head(.SD,3),by=.(recipient_name,donor_name)]
-p4b_recipient_top3_bundle$count = 1
-p4b_recipient_top3_bundle_count = p4b_recipient_top3_bundle[,.(top3count=sum(count)),by=.(recipient_name,oda_bundle_name)]
-p4b_recipient_top_bundle = merge(p4b_recipient_top1_bundle_count,p4b_recipient_top3_bundle_count,all=TRUE)
+# Function conditional_attribute_count:
+  # Arguments: crs - A data frame containing a year's worth of the full CRS data
+  #           attrib1 - A string containing the first attribute
+  #           attrib2 - A string containing the sector attribute
+  #           attrib3 - A string containing the third attribute
+# Output: A dataframe containing the number of times that the third attribute appears as
+# the top slice of unique combinations of the first and second attributes. This count is
+# also limited to the top 10 instances of the second attribute, such that the top1_count
+# should always sum to 10, and the top3 should always sum to 30. If there aren't 10 total categories
+# in attrib 1 (e.g. US's Bundle only has 7 bundles), top1 will sum to 7 and top3 will sum to 21
 
 conditional_attribute_count=function(crs,attrib1,attrib2,attrib3){
   p4b_recipient = data.table(crs)[
@@ -155,16 +84,18 @@ conditional_attribute_count=function(crs,attrib1,attrib2,attrib3){
     ,by=c(attrib1,attrib2)
     ]
   top10_donor_sector = top10_donor_sector[order(top10_donor_sector[,attrib1, with=F],-top10_donor_sector$usd_disb)]
-
+  
   top10_donor_sector = top10_donor_sector[,head(.SD,10),by=c(attrib1)]
   valid_combinations = paste(top10_donor_sector[,attrib1, with=F][[1]],top10_donor_sector[,attrib2, with=F][[1]])
-
+  
   p4b_recipient_top1_bundle= subset(p4b_recipient,recipient_sector %in% valid_combinations)
+  p4b_recipient_top1_bundle = p4b_recipient_top1_bundle[order(p4b_recipient_top1_bundle[,attrib1, with=F],p4b_recipient_top1_bundle[,attrib2, with=F],-p4b_recipient_top1_bundle$usd_disb)]
   p4b_recipient_top1_bundle = p4b_recipient_top1_bundle[,head(.SD,1),by=c(attrib1,attrib2)]
   p4b_recipient_top1_bundle$count = 1
   p4b_recipient_top1_bundle_count = p4b_recipient_top1_bundle[,.(top1count=sum(count)),by=c(attrib1,attrib3)]
-
+  
   p4b_recipient_top3_bundle = subset(p4b_recipient,recipient_sector %in% valid_combinations)
+  p4b_recipient_top3_bundle = p4b_recipient_top3_bundle[order(p4b_recipient_top3_bundle[,attrib1, with=F],p4b_recipient_top3_bundle[,attrib2, with=F],-p4b_recipient_top3_bundle$usd_disb)]
   p4b_recipient_top3_bundle = p4b_recipient_top3_bundle[,head(.SD,3),by=c(attrib1,attrib2)]
   p4b_recipient_top3_bundle$count = 1
   p4b_recipient_top3_bundle_count = p4b_recipient_top3_bundle[,.(top3count=sum(count)),by=c(attrib1,attrib3)]
@@ -172,13 +103,28 @@ conditional_attribute_count=function(crs,attrib1,attrib2,attrib3){
   return(p4b_recipient_top_bundle)
 }
 
+
+# counting top sectors by bundle ####
+p4b_donor_top_sector = conditional_attribute_count(crs,"donor_name","itep_sector","oda_bundle_name")
+
+# counting top bundles by sector ####
+p4b_donor_top_bundle = conditional_attribute_count(crs,"donor_name","oda_bundle_name","itep_sector")
+
+#recipient p4 starts here ####
+p4b_recipient_top_bundle = conditional_attribute_count(crs,"recipient_name","donor_name","oda_bundle_name")
+
+#recipient p4 starts here ####
+p4b_recipient_top_sector = conditional_attribute_count(crs,"recipient_name","donor_name","itep_sector")
+
 profile_tables = list(
   "P3 bundle"=p3_bundle,
   "P3 donor"=p3_donor,
   "P3 sector"=p3_sector,
   "P4a"=p4a,
   "p4b donor top sector"=p4b_donor_top_sector,
-  "p4b recip top bundle"=p4b_recipient_top_bundle
+  "p4b donor top bundle"=p4b_donor_top_bundle,
+  "p4b recip top bundle"=p4b_recipient_top_bundle,
+  "p4b recip top sector"=p4b_recipient_top_sector
   )
 table_names = names(profile_tables)
 wb = createWorkbook()
